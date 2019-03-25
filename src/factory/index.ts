84eb3ba2bd9d40f532a12c583598152cd9be0928
Guilherme Leal <lealhugui@gmail.com>
2019-03-25 10:27:45 -0300

    exposição de interfaces

--- src/factory/index.ts
+++ src/factory/index.ts
@@ -1,2 +1,3 @@
 export { NFeProcessor } from './processor/nfeProcessor'
 export { StatusServicoProcessor } from './processor/statusServicoProcessor'
+export * from './interface';
---
+++ src/factory/interface/index.ts
@@ -0,0 +1,2 @@
+export * from './evento';
+export * from './nfe';
