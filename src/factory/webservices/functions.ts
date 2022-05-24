dce6d7aa2b4e085b9d1a4fd0623eb71c4b0b578e
gestefane <gestefane@yahoo.com.br>
2022-05-23 23:11:04 -0300

    correcao de erro 415 (unsupported media type) nas requisicoes de NFCe para MG

--- src/factory/webservices/functions.ts
+++ src/factory/webservices/functions.ts
@@ -2,6 +2,8 @@ export function getContentType(uf: string): string {
     switch (uf) {
         case 'GO':
             return "application/soap+xml";
+        case 'MG':
+            return "application/soap+xml";
         default:
             return "text/xml;charset=utf-8";
     }
